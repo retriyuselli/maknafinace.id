07cf52d9fd1f0dc9eb0a253d74c00b555e5f171a
Rama Dhona Utama <ramadhona.utama@gmail.com>
2026-09-16 02:49:06 +0700

    Update

--- app/Services/FinanceSummaryService.php
+++ app/Services/FinanceSummaryService.php
@@ -209,7 +209,7 @@ class FinanceSummaryService
         $widgets = [
             [
                 'key' => 'customer_payments',
-                'title' => 'Total Pembayaran Pelanggan',
+                'title' => 'Total Pembayaran',
                 'value' => $this->formatOverviewMoney($customerPayments),
                 'value_raw' => $customerPayments,
                 'description' => 'Total pembayaran diterima',
@@ -217,7 +217,7 @@ class FinanceSummaryService
             ],
             [
                 'key' => 'customer_expenses',
-                'title' => 'Total Pengeluaran Pelanggan',
+                'title' => 'Total Pengeluaran',
                 'value' => $this->formatOverviewMoney($customerExpenses),
                 'value_raw' => $customerExpenses,
                 'description' => 'Total pengeluaran',
@@ -241,7 +241,7 @@ class FinanceSummaryService
             ],
             [
                 'key' => 'monthly_revenue',
-                'title' => 'Pendapatan Bulanan',
+                'title' => 'Revenue Bulanan',
                 'value' => $this->formatOverviewMoney($monthlyRevenue),
                 'value_raw' => $monthlyRevenue,
                 'description' => 'Pendapatan di '.$monthLabel,
